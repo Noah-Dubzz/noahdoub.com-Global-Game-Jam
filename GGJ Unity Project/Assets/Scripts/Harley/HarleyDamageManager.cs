29f26b1ec0296881173a09975b3819efcb10abf4
Micah <37186260+MicahRoberts@users.noreply.github.com>
2026-01-31 15:44:47 -0500

    Add bullet object pool and improve damage invulnerability
    Introduced bulletObjPool for efficient bullet reuse in Micah's scripts. Updated HarleyDamageManager and PerryDamageManager to use a new invulnerability flag and improved visual feedback during melee invulnerability. Set meleeInvulnerabilityTime to 3 in Harley and Perry prefabs.

--- GGJ Unity Project/Assets/Scripts/Harley/HarleyDamageManager.cs
+++ GGJ Unity Project/Assets/Scripts/Harley/HarleyDamageManager.cs
@@ -6,6 +6,7 @@ public class HarleyDamageManager : MonoBehaviour
     public float meleeInvulnerabilityTime = 1f;
     private HarleyPlayer _harleyPlayer = null;
     private float _invulTimer = 0f;
+    private bool _isInvul = false;
     
     private Renderer _renderer;
     private Material _harleyMat;
@@ -24,20 +25,30 @@ public class HarleyDamageManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_invulTimer <= Time.time)
+        if (_isInvul)
         {
-            _harleyMat.color = _basicColor;
-                
+            if (_invulTimer <= Time.time)
+            {
+                _renderer.enabled = true;
+                _isInvul = false;
+                _harleyMat.color = _basicColor;
+            }
+            else if(Time.time -(_invulTimer - meleeInvulnerabilityTime) > 0.25f)
+            {
+                _harleyMat.color = _basicColor;
+                _renderer.enabled = (Time.time % 0.5f) < 0.4f;
+            }
         }
     }
 
     public void MeleeDamage(float damage)
     {
-        if (Time.time >= _invulTimer)
+        if (Time.time >= _invulTimer && !_isInvul)
         {
             _harleyPlayer.Health -= damage;
             _invulTimer = Time.time + meleeInvulnerabilityTime;
             _harleyMat.color = Color.red;
+            _isInvul = true;
             
             Debug.Log(_harleyPlayer.Health);
         }
