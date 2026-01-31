bb209c7328ddee6695dffc05632e8cca5810f40f
AvocadoDisaster <and80800@gmail.com>
2026-01-31 01:56:59 -0500

    Add MaskManager and refactor mask-flip logic
    Introduce a new MaskManager (and .meta) to centralize mask-switch coordination and add it to SampleScene. Refactor HarleyPlayer and PerryPlayer: add static Instance singletons and Awake(), remove per-player flip/state logic, and make each player only set a MaskFlipConsent flag (with debug logs) when input is performed. Update prefabs: set both Harley and Perry prefabs' tag to "Player" and wire cross-references so MaskManager and prefabs reference each other. MaskManager now finds player instances, checks both consent flags, toggles support/damage state booleans, and invokes mode methods (currently debug logs) to switch modes.

--- GGJ Unity Project/Assets/Scripts/Harley/HarleyPlayer.cs
+++ GGJ Unity Project/Assets/Scripts/Harley/HarleyPlayer.cs
@@ -6,31 +6,28 @@ public class HarleyPlayer : MonoBehaviour
 {
     [SerializeField] private float Health = 100f;
     [SerializeField] private float Damage = 5f;
-    
-    
-    
+    public static HarleyPlayer Instance;
+
+  
 
     public bool MaskFlipConsentH = false;
-    private bool supportM = true;
-    private bool DamageM = false;
-    [SerializeField] public GameObject PerryPlayer;
-
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        Instance = this;
+        
+    }
     void Start()
     {
-        // PerryPlayer = PerryPlayer.GetComponent<PerryPlayer>();
-        SupportMode();
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (MaskFlipConsentH && perryPlayer.MaskFlipConsentP == true)
-        {
-            FlipMask();
-            MaskFlipConsentH = false;
-        }
+        
     }
 
     public void MaskFlip(InputAction.CallbackContext context)
@@ -38,36 +35,11 @@ public class HarleyPlayer : MonoBehaviour
         if (context.performed)
         {
             MaskFlipConsentH = true;
+            Debug.Log("Harley has consented for a mask switch");
 
             
         }
     }
 
-    private void FlipMask()
-    {
-        if (supportM)
-        {
-            supportM = false;
-
-            DamageMode();
-            DamageM = true;
-        }
-
-        if (DamageM)
-        {
-            supportM = true;
-            SupportMode();
-            DamageM = false;
-        }
-    }
-    private void SupportMode()
-    {
-        Debug.Log($"<collor=pink> Harley is in support Mode</color>");
-
-    }
-
-    private void DamageMode()
-    {
-        Debug.Log($"<collor=purple> Harley is in Damage Mode</color>");
-    }
+   
 }
